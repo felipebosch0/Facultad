c90924b47062435b367973e95992f47556103cb6
fbosch <felipebosch0@gmail.com>
2022-05-30 18:59:43 -0300

    Refactor and complete exercise

---
+++ Info1/vectores/Vector 5/Vector 2/Vector 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+/*2. Realice un programa que permita cargar 5 números enteros en un vector. 
+	Luego calcule el promedio de ellos. Finalmente mostrar todos los valores y su promedio.*/
+	int vec[5]= {0};
+	float prom= 0;
+	for (int ii=0;ii<5;ii++){
+		printf ("Ingrese un numero entero: ");
+		scanf ("%d", &vec[ii]);
+	}
+	prom = (vec[0]+vec[1]+vec[2]+vec[3]+vec[4])/5;
+	printf ("\nValor 1: %d\nValor 2: %d\nValor 3: %d\nValor 4: %d\nValor 5: %d\n",vec[0],vec[1],vec[2],vec[3],vec[4]);
+	printf ("\nPromedio: %f", prom);
+	return 0;
+}
+
--- estruct_selectivas/switch/P5.cpp
+++
@@ -1,4 +0,0 @@
-// ENUNCIADO:
-// Una compañía de envío internacional tiene servicio en algunos países del mundo.
-//El costo por el servicio de envío se basa en el peso del paquete y la zona a la que va dirigido. 
-
